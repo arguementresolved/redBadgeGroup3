42e365232f82f69e235c4e4a15c6649ed99d3481
Joshua Serie <serie1jd@gmail.com>
2019-04-09 15:22:31 -0400

    group push

--- HvH/src/app/services/auth.service.ts
+++ HvH/src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 const apiUrl = 'http://127.0.0.1:5000/';
 
@@ -12,4 +14,8 @@ export class AuthService {
   createNote(note: Note) {
     return this._http.post(`${apiUrl}/api/Notes`, note, { headers: this.getHeaders()});
   }
+
 }
+export class NotesService {
+
+  constructor(private _http: HttpClient) { };
