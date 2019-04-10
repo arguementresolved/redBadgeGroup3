39412a3999603e66d3c157634340d1b66c82932c
Logan Chaves <loganchaves@icloud.com>
2019-04-10 11:09:57 -0400

    got that shit

--- FlaskAPI/src/app.py
+++ FlaskAPI/src/app.py
@@ -5,6 +5,10 @@ from .models import db, bcrypt
 from .config import app_config
 
 
+from .views.user_view import user_api as user_blueprint
+from .views.blogpost_view import blogpost_api as blog_blueprint
+
+
 def create_app(env_name):
     '''
     Create App
@@ -16,7 +20,7 @@ def create_app(env_name):
     app.config.from_object(app_config[env_name])
 
     app.register_blueprint(user_api, url_prefix='/api/v1/users')
-    # app.register_blueprint(blog_blueprint, url_prefix='/api/v1/blogpost')
+    app.register_blueprint(blog_blueprint, url_prefix='/api/v1/blogpost')
 
     bcrypt.init_app(app)
     db.init_app(app)
