648f911e0f104751ff45b705b39c32b37ccd305b
Logan Chaves <loganchaves@icloud.com>
2019-04-11 10:51:48 -0400

    git fix

--- FlaskAPI/src/models/comments.py
+++ FlaskAPI/src/models/comments.py
@@ -6,25 +6,18 @@ from marshmallow import fields, Schema
 
 
 class BlogPostModel(db.Model):
-    '''
-    BlogPost Model
-    '''
+    
 
     __tablename__ = 'blogposts'
 
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    title = db.Column(db.String(128), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
     def __init__(self, data):
-        '''
-        Takes in a json request body
-        as data and parses to
-        instance attributes
-        '''
+     
 
         self.owner_id = data.get('owner_id')
         self.title = data.get('title')
@@ -36,24 +29,17 @@ class BlogPostModel(db.Model):
         return f'<id {self.id}>'
 
     def delete(self):
-        '''
-        deletes current model from database
-        '''
+    
         db.session.delete(self)
         db.session.commit()
 
     def save(self):
-        '''
-        saves new object to database
-        '''
+        
         db.session.add(self)
         db.session.commit()
 
     def update(self, data):
-        '''
-        updates model after setting attributes
-        and persists them to database
-        '''
+        
         for key, item in data.items():
             setattr(self, key, item)
         self.modified_at = datetime.utcnow()
