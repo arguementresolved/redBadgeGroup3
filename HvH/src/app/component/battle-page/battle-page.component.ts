f14217ea097f2c028922cc79ed08733fd93e1dc0
Joshua Serie <serie1jd@gmail.com>
2019-04-11 08:59:52 -0400

    Start of the day data sync

--- HvH/src/app/component/battle-page/battle-page.component.ts
+++ HvH/src/app/component/battle-page/battle-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { template } from '@angular/core/src/render3';
-import { BattlesID } from '../../models/battles';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-battle-page',
@@ -10,9 +10,21 @@ import { BattlesID } from '../../models/battles';
 )
 export class BattlePageComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(_form: FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit() {
   }
-  
+  createForm() {
+    this._registerForm = group({
+      fighter1: new FormControl,
+      fighter2: new FormControl
+    });
+
+  }
+  onSubmit() {
+    console.log(this._registerForm.value);
+  }
 }
